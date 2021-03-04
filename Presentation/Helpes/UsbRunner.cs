24097bd17232ee6fefec3a05cba50efb5ee1272a
davidpucknell <davidpucknell@users.noreply.github.com>
2021-03-04 08:15:06 -0800

    Really really basic timing functionality done

---
+++ Presentation/Helpes/UsbRunner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Helpes
+{
+    public class UsbRunner
+    {
+        private SerialPort usb;
+        public UsbRunner(String port)
+        {
+            usb = new SerialPort();
+            //set default parameters.
+            usb.PortName = port;
+            usb.ReadTimeout = 500;
+            usb.WriteTimeout = 100000;
+            usb.BaudRate = 1200;//need for the mode.
+            usb.Parity = Parity.None;
+            usb.StopBits = StopBits.One;
+            usb.DataBits = 8;
+            usb.Handshake = Handshake.None;
+            usb.RtsEnable = true;
+        }
+
+        public bool Setup(SerialDataReceivedEventHandler theHandler)
+        {
+            usb.DataReceived += theHandler;
+            usb.Open();
+
+            return true;
+        }
+
+    }
+}
---
+++ Presentation/Models/CarData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Models
+{
+    public class CarData
+    {
+        public List<CarTimeRow> data { get; set; }
+        public SerialPort usb { get; set; }
+        public String current { get; set; }
+    }
+}
